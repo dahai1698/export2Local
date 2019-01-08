6231e9a8c48bc22a75fc8fec40ee13433fe9ec1a
dahai1698 <summer.mkm@aliyun.com>
2019-01-08 18:07:40 +0800

    out 文件

---
+++ src/Export2LoalAction.java
@@ -0,0 +1,17 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.vfs.VirtualFile;
+
+public class Export2LoalAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        VirtualFile[] data = e.getData(DataKeys.VIRTUAL_FILE_ARRAY);
+        Export2LocalDialog dialog = new Export2LocalDialog(e);
+        dialog.setSize(628, 421);
+        dialog.setLocationRelativeTo(null);
+        dialog.setVisible(true);
+        dialog.requestFocus();
+    }
+}
